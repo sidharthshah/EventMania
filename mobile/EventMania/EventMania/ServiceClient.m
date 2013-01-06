e29263e7c84bc193c0c2a1ab8196b48c4a80c03c
Sidharth Shah <sidharth@fafadiatech.com>
2013-01-06 23:19:36 +0530

    implemented API call for auth

--- mobile/EventMania/EventMania/ServiceClient.m
+++ mobile/EventMania/EventMania/ServiceClient.m
@@ -7,13 +7,29 @@
 //
 
 #import "ServiceClient.h"
-#import "AFNetworking.h" 
-#import "JSONKit.h" 
+#import "AFNetworking.h"
+#import "JSONKit.h"
 
 @implementation ServiceClient
 
+NSString* BASE_URL = @"http://127.0.0.1:8000/api";
+
 -(BOOL) authUser:(NSString*)username password:(NSString*)password{
     NSLog(@"Called Auth User");
+    NSDictionary* params = [NSDictionary dictionaryWithObjectsAndKeys:
+                            @"username", @"iamsidd",
+                            @"passwd", @"k4k4Sidd",
+                            nil];
+    
+    AFHTTPClient* client = [[AFHTTPClient alloc] initWithBaseURL:[NSURL URLWithString:BASE_URL]];
+    [client registerHTTPOperationClass:[AFJSONRequestOperation class]];
+    [client postPath:@"auth" parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSLog(@"Success");
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"Fail");
+        NSLog(@"Request Failed with Error: %@, %@", error, error.userInfo);
+    }];
+    
     return TRUE;
 }
 @end
