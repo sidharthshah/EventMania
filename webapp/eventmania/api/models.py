869b4c79aff6bb3a593ea113a5e2831eb37bb798
Sidharth Shah <sidharth@fafadiatech.com>
2013-01-06 22:21:48 +0530

    implemented auth API

--- webapp/eventmania/api/models.py
+++ webapp/eventmania/api/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+from couchdb import *
 
-# Create your models here.
+class Account:
+    """
+    This class is used to handle all account related activities
+    """
+
+    def auth(self, username, passwd):
+        """
+        This method is used to authenticate user
+        """
+        couch = Server()
+        event_mania_users = couch["event_mania_users"]
+        if not event_mania_users.__contains__(username) or type(username) == type(None):
+            return False
+        return True
--- webapp/eventmania/api/views.py
+++ webapp/eventmania/api/views.py
@@ -1,1 +1,19 @@
-# Create your views here.
+
+from api.models import * 
+
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpRequest, HttpResponse
+from django.utils import simplejson
+
+
+@csrf_exempt
+def auth(request):
+    """
+    This method is used to authenticate user
+    """
+    a = Account()
+    data = {"response": {}}
+    username = request.POST.get("username")
+    passwd = request.POST.get("passwd")
+    data["response"]["status"] = a.auth(username, passwd)
+    return HttpResponse(simplejson.dumps(data), mimetype="application/json")
